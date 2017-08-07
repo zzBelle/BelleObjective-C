e0b9bf899b73bc2176e160a38a9e5d98bcb3774c
J <shiyue@shiyuedeMacBook-Pro.local>
2017-08-07 18:43:19 +0800

    OC基础

---
+++ WorkSummary/WorkSummary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WorkSummary
+//
+//  Created by 十月 on 2017/8/7.
+//  Copyright © 2017年 October. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
